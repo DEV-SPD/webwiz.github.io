211c170da28d40bb8e1050142fdbf30894c83a7e
DEV-SPD <103878703+DEV-SPD@users.noreply.github.com>
2022-04-17 16:50:15 +0530

    webtask

---
+++ main.js
@@ -0,0 +1,17 @@
+let eventtarget= document.getElementById('btn');
+
+function login(){
+    confirm("YOU ARE LOGGED IN!!");
+}
+
+
+
+let target = document.getElementById('btn1');
+
+function forgot(){
+    prompt("ENTER YOUR VERIFIED PHONE NUMBER:");
+    prompt("ENTER OTP:");
+    prompt("ENTER NEW PASSWORD:");
+    confirm("YOUR PASSWORD HAS BEEN RESET");
+}
+
